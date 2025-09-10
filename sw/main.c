e181728b25c5d00e2263ed766e57b371b4dcddcc
Arya-parab <aranpa090204@gmail.com>
2025-09-10 09:35:29 +0530

    Create main.c

---
+++ sw/main.c
@@ -0,0 +1,8 @@
+// Placeholder firmware for Microwatt accelerator
+#include <stdio.h>
+
+int main() {
+    printf("Microwatt Smart Sensor Project - Placeholder\n");
+    // TODO: Add sensor data processing
+    return 0;
+}
